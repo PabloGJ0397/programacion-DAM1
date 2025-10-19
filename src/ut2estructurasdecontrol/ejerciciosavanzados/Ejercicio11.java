e76b38d449ceb9543224e35f04566faeb13d764e
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2025-10-20 01:32:07 +0200

    UT2. EjerciciosAvanzados feat: 5,8,9. missing: 11

---
+++ src/ut2estructurasdecontrol/ejerciciosavanzados/Ejercicio11.java
@@ -0,0 +1,12 @@
+package ut2estructurasdecontrol.ejerciciosavanzados;
+
+/*
+Programa un juego que genere un número aleatorio y te permita introducir números hasta que des con el adecuado.
+El juego deberá indicarte si el número introducido es mayor o menor.
+ */
+public class Ejercicio11 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/ut2estructurasdecontrol/ejerciciosavanzados/Ejercicio5.java
@@ -0,0 +1,29 @@
+package ut2estructurasdecontrol.ejerciciosavanzados;
+
+import java.util.Scanner;
+
+/*
+Desarrolla un programa que solicite un número n y luego muestre por pantalla la siguiente figura:
+1
+1 2
+1 2 3
+1 2 3 4
+ 1 2 3 4 5 ………….
+ 1 2 3 4 5 …..n
+ */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        System.out.println("Introduce un número entero mayor que 0: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
