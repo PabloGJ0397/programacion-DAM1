724719959eed3502d37940138935624b70ee931d
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2025-10-20 20:38:30 +0200

    Terminando ejercicio 11. Falta revisar

--- src/ut2estructurasdecontrol/ejerciciosavanzados/Ejercicio8.java
+++ src/ut2estructurasdecontrol/ejerciciosavanzados/Ejercicio8.java
@@ -6,6 +6,7 @@ Muestra los números primos entre 1 y 100.
 public class Ejercicio8 {
     public static void main(String[] args) {
         // recorre los números del 2 (el 1 no es primo) al 100//
+
         for (int i = 2; i < 100; i++) {
             // variable contador, cuando sea = 2 significará que el número es primo.
             int count = 0;
