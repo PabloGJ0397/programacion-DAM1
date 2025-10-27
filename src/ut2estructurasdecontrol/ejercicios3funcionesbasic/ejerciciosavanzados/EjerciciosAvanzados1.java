bdadb7ac1ceae363c112bd2c740c5eca4cae8bfd
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2025-10-27 19:49:54 +0100

    Fix: ejerciciosbucles.ejerciciosbásicos.Ejercicio15

---
+++ src/ut2estructurasdecontrol/ejercicios3funcionesbasic/ejerciciosavanzados/EjerciciosAvanzados1.java
@@ -0,0 +1,13 @@
+package ut2estructurasdecontrol.ejercicios3funcionesbasic.ejerciciosavanzados;
+//Implementa un método recursivo que determine si un número es primo.
+public class EjerciciosAvanzados1 {
+    public static void main() {
+
+    }
+    public static boolean esPrimo (int numero){
+
+
+
+        }
+    }
+}
---
+++ src/ut2estructurasdecontrol/ejercicios3funcionesbasic/ejerciciosavanzados/EjerciciosAvanzados8.java
@@ -0,0 +1,4 @@
+package ut2estructurasdecontrol.ejercicios3funcionesbasic.ejerciciosavanzados;
+
+public class EjerciciosAvanzados8 {
+}
